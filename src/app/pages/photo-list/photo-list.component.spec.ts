82373007ce71a355d39dece889e6e9843bd54d43
Daniel Mendes Sechinske <mendes.sechinske@gmail.com>
2022-09-27 13:44:31 -0300

    Fixed PhotoListComponent test that was not working as expected

--- src/app/pages/photo-list/photo-list.component.spec.ts
+++ src/app/pages/photo-list/photo-list.component.spec.ts
@@ -1,21 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { PhotoListComponent } from './photo-list.component';
 import { PhotoListModule } from './photo-list.module';
-import { HttpClientModule } from '@angular/common/http';
 import { PhotoBoardService } from '../../shared/services/photo-board/photo-board.service';
-import { buildPhotoList } from '../../shared/components/photo-board/test-helper/build-photo-list';
-import { of } from 'rxjs';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+const mockData = {
+  api: 'http://localhost:3000/photos',
+  data: [
+    {
+      id: 1,
+      description: 'example 1',
+      src: ''
+    },
+    {
+      id: 2,
+      description: 'example 2',
+      src: ''
+    }
+  ]
+}
 
 describe('PhotoListComponent', () => {
   let component: PhotoListComponent;
   let fixture: ComponentFixture<PhotoListComponent>;
   let service: PhotoBoardService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [
         PhotoListModule,
-        HttpClientModule
+        HttpClientTestingModule
       ]
     })
       .compileComponents();
@@ -23,6 +38,7 @@ describe('PhotoListComponent', () => {
     fixture = TestBed.createComponent(PhotoListComponent);
     component = fixture.componentInstance;
     service = TestBed.inject(PhotoBoardService);
+    httpTestingController = TestBed.inject(HttpTestingController)
     fixture.detectChanges();
   });
 
@@ -31,13 +47,19 @@ describe('PhotoListComponent', () => {
   });
 
   it('(DOM) should display photos when data arrives', () => {
-    const photos = buildPhotoList();
-    spyOn(service, 'getPhotos').and.returnValue(of(photos));
+    httpTestingController.expectOne(mockData.api).flush(mockData.data);
     fixture.detectChanges();
     const boardElement = fixture.nativeElement.querySelector('app-photo-board');
     const loadingElement = fixture.nativeElement.querySelector('.loading');
-    // expect(boardElement).withContext('Should display board element').not.toBeNull();
-    // expect(loadingElement).withContext('Should not display loading element').toBeNull();
-    expect(true).toBe(true);
+    expect(boardElement).withContext('Should display board element').not.toBeNull();
+    expect(loadingElement).withContext('Should not display loading element').toBeNull();
   })
+
+  it('(DOM) should display loading element while data is not present', () => {
+    fixture.detectChanges();
+    const boardElement = fixture.nativeElement.querySelector('app-photo-board');
+    const loadingElement = fixture.nativeElement.querySelector('.loading');
+    expect(boardElement).withContext('Should display board element').toBeNull();
+    expect(loadingElement).withContext('Should not display loading element').not.toBeNull();
+  });
 });
