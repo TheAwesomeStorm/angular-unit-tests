11678bbbceed546cb16cd69e6b4ccc208a6f9026
Daniel Mendes Sechinske <mendes.sechinske@gmail.com>
2022-09-26 23:11:45 -0300

    Created PhotoBoardComponent

---
+++ src/app/shared/components/photo-board/photo-board.component.ts
@@ -0,0 +1,34 @@
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Photo } from '../../interfaces/photo';
+
+@Component({
+  selector: 'app-photo-board',
+  templateUrl: './photo-board.component.html',
+  styleUrls: ['./photo-board.component.scss']
+})
+export class PhotoBoardComponent implements OnInit, OnChanges {
+
+  @Input() public photos: Photo[] = [];
+  public rows: any[][] = [];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  ngOnChanges (changes: SimpleChanges) {
+    if(changes['photos']) {
+      this.rows = this.groupColumns(changes['photos'].currentValue);
+    }
+  }
+
+  public groupColumns(photos: Photo[]): any[][] {
+    const newRows = [];
+    const rowStep = 4;
+    for (let i = 0; i < photos.length; i += rowStep) {
+      newRows.push(photos.slice(i, i + rowStep));
+    }
+    return newRows;
+  }
+
+}
